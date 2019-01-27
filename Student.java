cfba4e8c2b0509f4c57430504130e09780385053
Seweryn Krupiński <45992679+sewerynkrupinski@users.noreply.github.com>
2019-01-27 16:07:34 +0100

    dodane komentarze

--- Student.java
+++ Student.java
@@ -5,16 +5,16 @@ public class Student {
     private String imie;
     private String nazwisko;
     private int index;
-    public static int count = 0;
+    public static int count = 0; //deklaracja licznika studentów
 
 
-    public Student(String imie, String nazwisko, int index){
+    public Student(String imie, String nazwisko, int index){ //konstruktor
 
         setImie(imie);
         setNazwisko(nazwisko);
         setIndex(index);
 
-        count += 1;
+        count += 1; //po utworzeniu obiektu dodaje do licznika nowego studenta
 
     }
 
@@ -26,6 +26,9 @@ public class Student {
                 ", index=" + index +
                 '}';
     }
+
+
+    // gettery i setery
 
     public void setImie(String imie){
         this.imie = imie;
--- StudentController.java
+++ StudentController.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 
 public class StudentController {
 
-    ArrayList<Student> studentList = new ArrayList<>();
+    ArrayList<Student> studentList = new ArrayList<>(); //utworzenie listy studentow (obiektow typu Student)
     Scanner input = new Scanner(System.in);
 
     public StudentController(){
-        boolean flag = true;
+        boolean flag = true; //zastosowano flage bo switch po wybraniu q nie moglby opuscic petli
         while(flag){
             System.out.println("A - dodaj, S - pokaż, D - usuń, U - zmodyfikuj, C - liczba studentów, Q - wyjście");
             String wybor = input.nextLine().toUpperCase();
@@ -50,8 +50,8 @@ public class StudentController {
         String nazwisko = input.nextLine();
         System.out.println("Podaj index studenta");
         int index = input.nextInt();
-        input.nextLine();
-        studentList.add(new Student(imie, nazwisko, index));
+        input.nextLine(); //konsumuje enter
+        studentList.add(new Student(imie, nazwisko, index));  //dodaje nowy obiekt do listy, z podanymi parametrami
 
     }
 
@@ -59,17 +59,17 @@ public class StudentController {
         System.out.println("Podaj index studenta do usunięcia");
         int index = input.nextInt();
         input.nextLine();
-        for (Student val : studentList){
-            if (val.getIndex() == index) {
-                studentList.remove(val);
-                Student.count -=1;
+        for (Student val : studentList){ //przeglada liste studentow i dany obiekt zapisuje do tymczasowej zmiennej val
+            if (val.getIndex() == index) { //pobiera z danego obiektu indeks i sprawdza czy jest rowny z wprowadzonym z klawiatury i jeśli tak...
+                studentList.remove(val); //... to usuwa dany obiekt
+                Student.count -=1;      //pomniejsza licznik studentow o 1
                 break;
             }
         }
 
     }
 
-    public void showStudent(){
+    public void showStudent(){ //wyświetla listę wszystkich studentow
 
         for (Student tmp : studentList){
             System.out.println("imię: "+tmp.getImie()+ " ,nazwisko: " +tmp.getNazwisko()+ " ,index: " +tmp.getIndex());
@@ -79,12 +79,12 @@ public class StudentController {
 
     }
 
-    public void countStudent(){
+    public void countStudent(){ //wyswietla liczbe studentow
 
         System.out.println("Liczba studentów: " +Student.count);
     }
 
-    public void modStudent(){
+    public void modStudent(){  //modyfikacja studenta
 
         System.out.println("Podaj index studenta do modyfikacji");
         int index = input.nextInt();
@@ -94,22 +94,19 @@ public class StudentController {
         System.out.println("Podaj nazwisko");
         String nazwisko = input.nextLine();
 
-        for (Student val2 : studentList){
-            if (val2.getIndex() == index){
-                if (!imie.equals("")){
+        for (Student val2 : studentList){  //przeglada liste studentow
+            if (val2.getIndex() == index){ //z kazdego obiektu sprawdza indeks i sprawdza czy ten indeks jest taki sam jak wprowadzony z klawiatury
+                if (!imie.equals("")){ //jesli wprowadzony jest inny niz pusty to zmienia imie z uzyciem setera
                     val2.setImie(imie);
                 }
-                if (!nazwisko.equals("")){
+                if (!nazwisko.equals("")){   //jesli wprowadzone nazwisko jest rozne od pustego to ustawia nowe naziwsko
                     val2.setNazwisko(nazwisko);
                 }
 
                 break;
             }
         }
-
-
     }
-
 }
 
 
